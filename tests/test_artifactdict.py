6eb2c8c10add5b847dd65e6eeba85f2765a569a0
Suhail Rehman <suhailrehman@gmail.com>
2021-05-29 10:57:00 +0530

    Artifact dict implemention complete and tests pass

--- tests/test_artifactdict.py
+++ tests/test_artifactdict.py
@@ -3,22 +3,36 @@ import os
 import logging
 
 import pytest
+import numpy as np
+import pandas as pd
 
 from relic.utils.artifactdict import ArtifactDict
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-data_dir = os.path.join(THIS_DIR, '../data/20210126-153738/artifacts/')
+data_dir = os.path.join(THIS_DIR, 'data/20210126-153738/artifacts/')
 files = glob.glob(data_dir+'/*.csv')
 
 
 @pytest.fixture
 def artifactdict_instance():
     """Returns an ArtifactDict initialized to test_dir"""
-    logging.info('Testing artifactdir on input:' + str(data_dir))
+    logging.info('Testing artifactdir on input: ' + str(data_dir))
+    logging.debug('File List: ' + str(files))
     return ArtifactDict(data_dir)
 
 
 def test_keys(artifactdict_instance):
-    actual_files = set(os.path.basename(f) for f in files)
-    test_files = set(artifactdict_instance.keys())
-    assert actual_files == test_files
+    expected_files = set(os.path.basename(f) for f in files)
+    actual_files = set(artifactdict_instance.keys())
+    logging.debug('Keys: ' + str(expected_files))
+    assert len(expected_files) != 0
+    assert actual_files == expected_files
+
+
+def test_missing(artifactdict_instance):
+    test_file = np.random.choice([os.path.basename(f) for f in files], 1)[0]
+    expected_columns = set(pd.read_csv(data_dir + test_file, index_col=0).columns)
+    actual_columns = set(artifactdict_instance[test_file].columns)
+    logging.debug('Expected Columns: ' + str(expected_columns))
+    assert len(expected_columns) != 0
+    assert actual_columns == expected_columns
