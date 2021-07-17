3c54542702d4ac85ab25fe17f4dc45e3e4893c6c
Suhail Rehman <suhailrehman@users.noreply.github.com>
2021-07-17 16:37:14 +0530

    Webserver untars canned demos if they don't already exist

--- relic/server/__init__.py
+++ relic/server/__init__.py
@@ -7,9 +7,10 @@ RELIC_TMP_LOCATION = "/tmp/relic/"
 import tarfile
 import os
 os.makedirs(RELIC_TMP_LOCATION, exist_ok=True)
-tar = tarfile.open(CANNED_DEMO_LOCATION)
-tar.extractall(path="/tmp/relic/")
-tar.close()
+if not os.path.exists(RELIC_TMP_LOCATION+'githubviz'):
+    tar = tarfile.open(CANNED_DEMO_LOCATION)
+    tar.extractall(path="/tmp/relic/")
+    tar.close()
 
 
 if __name__ == '__main__':
